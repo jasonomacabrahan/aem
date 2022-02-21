b488c6f80eb9990bd88fdd2c85d693a67bc39825
VirgilFuentes <89828194+VirgilFuentes@users.noreply.github.com>
2022-02-21 14:21:53 +0800

    Tasks assignments

---
+++ database/migrations/2022_02_21_041406_create_task_assignments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_assignments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('papID')->default(0);
+            $table->integer('taskBy')->default(0);
+            $table->string('taskedTo')->nullable();
+            $table->string('taskDetail')->nullable();
+            $table->integer('taskResolved')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_assignments');
+    }
+}
