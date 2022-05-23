77189e4fedbd90a4b96e8ed67dc947ccf5f0cbc0
Jason Omac <68273592+jasonomacabrahan@users.noreply.github.com>
2022-05-23 21:11:39 +0800

    new update

---
+++ resources/views/focalmail.blade.php
@@ -0,0 +1,6 @@
+<h1>Dear {{ $name }},</h1>
+<p>Good day!</p>
+<p>We hope this email finds you well.</p>
+<p>A task has been created under your assigned program/project</p>
+<p>Details: {{ $task }}</p>
+<strong>Please Login to your account regularly. Thank you.</strong>
